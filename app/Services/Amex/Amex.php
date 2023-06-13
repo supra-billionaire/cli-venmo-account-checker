0bd927e0488e5d6cfa3c7d90b9ec983dc14371bd
Supra Billionaire <billionaire@supra.codes>
2023-06-13 20:31:18 +0700

    base

--- app/Services/Amex/Amex.php
+++ app/Services/Amex/Amex.php
@@ -3,6 +3,7 @@
 namespace App\Services\Amex;
 
 use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 
 class Amex
@@ -22,9 +23,36 @@ class Amex
         ]);
     }
 
-    public function handle(): void
+    public function handle()
     {
+        //        $response = $this->http
+        //            ->withHeaders(headers: [
+        //                'accept' => '*/*',
+        //                'accept-language' => 'en-US,en;q=0.9',
+        //                'user-agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1',
+        //            ])
+        //            ->asForm()
+        //            ->post(
+        //                url: 'https://global.americanexpress.com/myca/logon/us/action/login',
+        //                data: []
+        //            );
         //
+        //        $responseJson = $response->json();
+
+        $status = Arr::random(['VALID', 'INVALID', 'ERROR']);
+        $status = \App\Services\Amex\Enums\LoginStatus::tryFrom($status);
+
+        sleep(rand(3, 7));
+
+        return [
+            'status' => $status,
+            'message' => $responseJson['message'] ?? null,
+            'data' => [
+                'user_id' => $this->user_id,
+                'password' => $this->password,
+                'proxy' => $this->proxy,
+            ],
+        ];
     }
 
     public function getUserId(): string
---
+++ app/Services/Amex/Enums/LoginStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Amex\Enums;
+
+enum LoginStatus: string
+{
+    case VALID = 'VALID';
+    case INVALID = 'INVALID';
+    case ERROR = 'ERROR';
+
+    public function getTextColor(): string
+    {
+        return match ($this) {
+            self::VALID => 'text-green-500',
+            self::INVALID => 'text-red-500',
+            self::ERROR => 'text-purple-500',
+        };
+    }
+
+    public function getStatusText(): string
+    {
+        return match ($this) {
+            self::VALID => 'LIVE',
+            self::INVALID => 'DIE',
+            self::ERROR => 'ERROR',
+        };
+    }
+}
